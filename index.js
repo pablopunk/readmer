dd7b35a6d64d31457bee39753f6e8c6563e74c52
Pablo Varela <pablovarela182@gmail.com>
2017-05-31 22:37:48 -0400

    Default picture and global paths

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const getMarkdown = async ({name, description, author}) => {
   return `
 # ${name}
@@ -22,15 +24,14 @@ Feel free to open an _issue_ or a _PR_.
 
 ## Author
 
-| ![me](https://www.gravatar.com/avatar/fa50aeff0ddd6e63273a068b04353d9d?s=100) |
+| ![me](https://www.lightstim.com/images/pro/icon-youraccount.jpg) |
 | ----------------------------------------------------------------------------- |
-| © 2017 __${author}__ |
-
+| © 2017 [__${author}__]() |
 `
 }
 
-module.exports = async (packagePath = 'package.json') => {
-  const package = require(`./${packagePath}`)
+module.exports = async (packagePath = './package.json') => {
+  const package = require(`${packagePath}`)
   if (!package) {
     throw Error(`${packagePath} not found`)
   }
--- test.js
+++ test.js
@@ -5,3 +5,8 @@ test('Returns output', async t => {
   t.plan(1)
   await readmer().then(output => t.pass())
 })
+
+test('Handles total path', async t => {
+  t.plan(1)
+  await readmer(`${process.cwd()}/package.json`).then(output => t.pass())
+})
